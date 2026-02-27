981125c210a8a946dbc9322d19aa5b26e9793a95
Focus_Raspi <siwakorn@gm.kochi-ct.jp>
2026-02-27 10:20:41 +0900

    Orignal script added

--- README.md
+++ README.md
@@ -1,133 +1,27 @@
-# MPU9250-sidereal_day
+# MPU-9250 with Raspberry Pi
+This program provides basic functionality of the MPU-9250 with a Raspberry Pi over an I<sup>2</sup>C interface. The program uses the [pigpio](https://abyz.me.uk/rpi/pigpio/index.html) library for the I<sup>2</sup>C read and write functionality.
+<p>This program was tested on a Raspberry Pi Zero W running Raspbian GNU/Linux 10 (buster).</p>
 
-9軸センサ-MPU9250から得られる地球の角速度データを用いて 「恒星日（地球が1回転する時間）」を推定する。
-
-## 詳細
-
-未投稿
-
-## はじめ
-
-### 開発環境
-
-* OS: Trixie (64-bit)
-* デバイス：Raspberry Pi 4, Raspberry Pi Zero(未確認)
-
-### インストール
-
-#### pigpioのインストール
-初めに、`pigpio`をインストールする。しかし、`TRIXIE`には提供されていないために、自分自身で`build`する。
-`pigpio`のパッケージをインストールするために、次のコマンドを実行する。
-```bash
-sudo apt install -y python3-setuptools python3-full
-wget https://github.com/joan2937/pigpio/archive/refs/tags/v79.tar.gz
-tar zxf v79.tar.gz
-cd pigpio-79
-make
-sudo make install
-sudo ldconfig
-```
-次に、`pigpio.service`のテンプレートを作成する。次のコマンドを実行する。（全部コピー・ペストしてください）
-```bash
-DOLLAR="\$"
-
-cat << EOF > config_pigpiod_service.sh
-#! /bin/bash
-PIGPIOD_SERVICE="/lib/systemd/system/pigpiod.service"
-
-if [[ ! -f "${DOLLAR}{PIGPIOD_SERVICE}" ]] ; then
-
-  # The configuration of pigpiod appears to be different on 32 bit and 64 bit systems
-  # pigpiod must be run with -t0 on a 64 bit system, and -t1 on a 32 bit system.
-  # The 32 bit behaviour contradicts what is documented in the pigpiod web documentation ;-(
-  ARCH=${DOLLAR}(uname -m)
-
-  if [[ "${DOLLAR}{ARCH}" == "aarch64" ]] ; then
-    T_COMMAND="-t0"
-  elif [[ "${DOLLAR}{ARCH}" == "armv7l" ]] ; then
-    T_COMMAND="-t1"
-  elif [[ "${DOLLAR}{ARCH}" == "armv6l" ]] ; then
-    T_COMMAND="-t1"
-  else
-    echo "uname -m returns '${DOLLAR}{ARCH}' which is an unexpected value. Time to debug..."
-    echo
-    echo "I have not seen armv8 on my Pi3, but I believe this might happen if running"
-    echo "64 bits. I am not sure whether pigpiod requires -t0 or -t1 in this case."
-    echo "You'll need to experiment."
-    echo
-    echo "I believe armv6 is a possibility on a Pi2 or earlier?"
-    echo "I guess use same settings as for armv6l?"
-    exit 1
-  fi
-
-  # We are going to overwrite the systemd configuration file for pigpiod
-  # because we are going to change the launch options
-  cat << END > "${DOLLAR}{PIGPIOD_SERVICE}"
-[Unit]
-Description=Daemon required to control GPIO pins via pigpio
-
-[Service]
-Type=forking
-ExecStart=/usr/local/bin/pigpiod -l -s10 ${DOLLAR}{T_COMMAND} -x0xFFFFFFF
-Restart=always
-ExecStop=/bin/systemctl kill pigpiod
-
-[Install]
-WantedBy=multi-user.target
-END
-else
-  echo "${DOLLAR}{PIGPIOD_SERVICE} already exists. I refuse to change it."
-fi
-EOF
-
-chmod 755 config_pigpiod_service.sh
-sudo ./config_pigpiod_service.sh
-```
-最後に`daemon`を再起動し、`pigpio.service`を起動する。
-```bash
-sudo systemctl daemon-reload
-sudo systemctl enable --now pigpiod.service
-```
-`pigpio`のバーションを確認する。
+## To install
+You would first need to check if you have pigpio installed. To do so, run the following line of code in the terminal.
 ```bash
 pigpiod -v
 ```
-[重要] 次に進む前に `pigpio.service`を一時停止する。
+If the command is not recognized you can [download and install pigpio](https://abyz.me.uk/rpi/pigpio/download.html).
+Once pigpio is installed, you may download and build this program. To do so, run the following lines in the terminal:
 ```bash
-sudo systemctl stop pigpiod
-sudo systemctl disable pigpiod
-sudo fuser -k 8888/tcp
-sudo rm -f /var/run/pigpio.pid
-```
-
-#### プロクラム本体
-このリポジトリを`clone`する。または、`.tar`のファイルをダウンロードし、展開する。
-```
-git clone https://github.com/4Cu5-creator/MPU9250-sidereal_day.git
+git clone https://github.com/Alexgichamba/mpu9250-with-raspberrypi.git
 cd mpu9250-with-raspberrypi/
 make
 ```
-### プロクラムの実行
-
-`~\MPU9250-sidereal_day`に入り、次のコマンドを実行する。
-```
+Then you may run the program:
+```bash
 sudo ./bin/mpu9250
 ```
 
-## Authors
-
-フォーカス
-
-
-## 参考リンク
-
-* [mpu9250-with-raspberrypi](https://github.com/Alexgichamba/mpu9250-with-raspberrypi/tree/main)
-* [Pigpiod not installing under new official trixie release](https://github.com/joan2937/pigpio/issues/632)
-* [それでも地球は回っている!](https://elchika.com/article/2e2a5d67-8ae6-4c7c-b0fe-ec6be2e10566/)
-
-テンプレート
-* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-* [dbader](https://github.com/dbader/readme-template)
-* [zenorocha](https://gist.github.com/zenorocha/4526327)
-* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
-
+## Functionality
+* [X] Read and output inertial measurment unit data
+* [X] Read and output temperature data
+* [X] Calculate and log maximums and minimums to a file
+* [ ] Read and output magnetometer data
+* [ ] Plot the data on graphs
--- src/main.cpp
+++ src/main.cpp
@@ -1,186 +1,104 @@
+/*
+    Author: Alex Gichamba
+*/
 #include <iostream>
-#include <time.h>
-#include <vector>
-#include <fstream> // For file input/output
 
-// Linux terminal control headers for detecting raw key presses
-#include <termios.h>
-#include <unistd.h>
-
-//MakeFile
 #include "../inc/i2c.h"
 #include "../inc/timer.h"
 #include "../inc/stats.h"
 #include "../inc/temp.h"
 #include "../inc/accel.h"
 #include "../inc/gyro.h"
-#include "../inc/geo.h"
 
-//Variables
-#define FIFO_Threshold 1
-#define Num_Reads 6
-#define Acquisition_Time 65
-#define IMU_Clock_Frequency 19200000.0
-
-int read_main(int device_handle, double& out_gx, double& out_gy, double& out_gz);
-
-double gyro_x[Num_Reads];
-double gyro_y[Num_Reads];
-double gyro_z[Num_Reads];
-
-// --- Helper Function to Wait for Spacebar ---
-void waitForSpace() {
-    std::cout << "Press the SPACEBAR to begin the next read..." << std::flush;
-    
-    struct termios oldt, newt;
-    // Get current terminal attributes
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    
-    // Disable canonical mode (buffering) and echo
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    char ch;
-    do {
-        ch = getchar();
-    } while (ch != ' '); // Keep looping until the space character is pressed
-
-    // Restore original terminal attributes
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    std::cout << "\n";
-}
 
 int main(int argc, char* argv[])
 {
-    //MPU9250 setup
-    if (gpioInitialise() < 0) return -1;
+    if (gpioInitialise()<0) //initialises pigpio.h
+    {
+        //if pigpio initialisation failed
+        std::cout<<"pigpio.h initialisation failed\n";
+        return -1;
+    }
 
-    int device_handle = i2cOpen(I2C_DEVICE, I2C_DEVICE_ADDR, I2C_FLAGS);
-    if (device_handle < 0) return -2;
+    //variables commonly used in main()
+    int device_handle, mag_device_handle, wake_handle, mag_wake_handle, append_handle;
+    std::string current_time;
+    double temp_degC;
+    double accel_x, accel_y, accel_z;
+    double gyro_x, gyro_y, gyro_z;
+    /* Magnetometer functionality is still in progress
+    double mag_x, mag_y, mag_z;
+    */
+    //attempt to open I2C devices
+    device_handle = i2cOpen(I2C_DEVICE,I2C_DEVICE_ADDR,I2C_FLAGS);
+    if (device_handle < 0)
+    {
+        std::string err = "Failed to open i2c communication to IMU\n";
+        std::cout<<err<<"\n";
+        return -2;
+    }
 
-    i2cWriteByteData(device_handle, PWR_MGMT_1_ADDR, PWR_MGMT_1_VAL);
-    set_gyro(device_handle, 250);
-    set_accel(device_handle, 2);
-
-    device_wait(100);
-    std::cout << "Starting Acquisition for " << Acquisition_Time << " seconds, " << Num_Reads << " times...\n";
-
-    // --- Setup CSV File ---
-    std::ofstream csv_file("imu_averages.csv");
-    if (!csv_file.is_open()) {
-        std::cout << "Failed to create CSV file!\n";
+    //attempt to disable sleep modes
+    wake_handle = i2cWriteByteData(device_handle,PWR_MGMT_1_ADDR,PWR_MGMT_1_VAL);
+    if (wake_handle<0)
+    {
+        std::string err = "Failed to wake IMU device\n";
+        std::cout<<err<<"\n";
         return -4;
     }
+    // Enable I2C Master mode
+    i2cWriteByteData(device_handle,USER_CTRL,0x20);
+    
+    
+    
 
-    csv_file << "Read_Number,Avg_Gx,Avg_Gy,Avg_Gz\n";
+    device_wait(100); //to allow device to be ready to take readings
+    std::cout << "Device is ready to take readings\n\n";
 
-    //Main Loop --> Reading Data
-    for (int i = 0; i < Num_Reads; i++) {
-        std::cout << "\n--- Starting Read " << (i + 1) << " of " << Num_Reads << " ---\n";
+    //attempt to set scales
+    set_gyro(device_handle,250);
+    set_accel(device_handle,2);
+    
+    //infinite loop to get readings from device
+    while (true)
+    {
+        //find the time now
+        current_time = get_time();
 
-        // Wait for the user to press the space key
-        waitForSpace();
-
-        // Variables to hold the results from this specific read
-        double current_gx, current_gy, current_gz;
-
-        if (read_main(device_handle, current_gx, current_gy, current_gz) != 0) {
-            std::cout << "An error occurred during measurement.\n";
-            csv_file.close();
-            return -3;
+        //get readings
+        temp_degC = get_temp(device_handle);
+        accel_x = get_accel_x(device_handle);
+        accel_y = get_accel_y(device_handle);
+        accel_z = get_accel_z(device_handle);
+        gyro_x = get_gyro_x(device_handle);
+        gyro_y = get_gyro_y(device_handle);
+        gyro_z = get_gyro_z(device_handle);
+        /* Magnetometer functionality is still in progress
+        mag_x = get_mag_x(mag_device_handle);
+        mag_y = get_mag_y(mag_device_handle);
+        mag_z = get_mag_z(mag_device_handle);
+        */
+        //output readings
+        std::cout<<"Time and date: "<<current_time;
+        std::cout << "Temp: "<<temp_degC<<"  [deg Celcius]\n";
+        std::cout << "Accel:  x: "<<accel_x<<"  y: "<<accel_y<<"  z: "<<accel_z<<"  [m/s^2]\n";
+        std::cout << "Gyro:  x: "<<gyro_x<<"  y: "<<gyro_y<<"  z: "<<gyro_z<<"  [deg/sec]\n";
+        /* Magnetometer functionality is still in progress
+        std::cout << "Mag:  x: "<<mag_x<<"  y: "<<mag_y<<"  z: "<<mag_z<<"  [uT]\n";
+        */
+        std::cout <<"\n";
+        
+        append_handle = append_readings(current_time,temp_degC,accel_x,accel_y,accel_z,gyro_x,
+                                        gyro_y,gyro_z /* ,mag_x,mag_y,mag_z */);       
+        if (append_handle < 0)
+        {
+            std::cout<< "Unable to append readings\n";
         }
-
-        //Saving Data in Array
-        gyro_x[i] = current_gx;
-        gyro_y[i] = current_gy;
-        gyro_z[i] = current_gz;
-
-        // Write the data to the CSV file
-        csv_file << (i + 1) << ","
-                 << current_gx << ","
-                 << current_gy << ","
-                 << current_gz << "\n";
-
-        std::cout << "finish\n";
+        
+        get_stats();
+        
+        device_wait(5000); //sleep for 5 sec
     }
-
-    // --- Calculations ---
-    double angular_velocity;
-    angular_velocity = radius_estimation(Num_Reads, gyro_x, gyro_y, gyro_z);
-
-    if (angular_velocity < 0) {
-       std::cout << "Calculation is incorrect\n";
-       return -5;
-    }
-
-    std::cout << angular_velocity << " rad/s\n" << std::endl;
-
-    double sid_day = sidereal_day(angular_velocity);
-
-    std::cout << "A sidereal day is " << sid_day << " hours long.\n" << std::endl;
-
-    // Close the file when done
-    csv_file.close();
-    std::cout << "\nAll data saved successfully to imu_averages.csv\n";
-
-    return 0;
-}
-
-//Read Data
-int read_main(int device_handle, double& out_gx, double& out_gy, double& out_gz ) {
-    struct timespec start, now, last_read;
-    double total_time = 0;
-    double g_x_sum = 0, g_y_sum = 0, g_z_sum = 0;
-
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    last_read = start;
-
-    // CountDown
-    for(int i = 3; i > 0; i--) {
-        std::cout << i << "...\n";
-        device_wait(1000);
-    }
-
-    //Integration
-    do {
-        clock_gettime(CLOCK_MONOTONIC, &now);
-        double dt = (now.tv_sec - last_read.tv_sec) +
-                    (now.tv_nsec - last_read.tv_nsec) / 1e9;
-        last_read = now;
-
-        double gx = get_gyro_x(device_handle);
-        double gy = get_gyro_y(device_handle);
-        double gz = get_gyro_z(device_handle);
-
-        //Deg/s --> Rad/s
-        gx = deg2rad(gx);
-        gy = deg2rad(gy);
-        gz = deg2rad(gz);
-
-        g_x_sum += gx * dt;
-        g_y_sum += gy * dt;
-        g_z_sum += gz * dt;
-        total_time += dt;
-
-        double elapsed = (now.tv_sec - start.tv_sec) +
-                         (now.tv_nsec - start.tv_nsec) / 1e9;
-
-        if ((int)elapsed % 5 == 0 && dt > 0) {
-             std::cout << "Acquiring... " << (int)elapsed << "s\r" << std::flush;
-        }
-
-        if (elapsed >= Acquisition_Time) break;
-
-    } while (true);
-
-    // Instead of creating new local variables, we assign the answers
-    // to the reference variables passed from main()
-    out_gx = g_x_sum / total_time;
-    out_gy = g_y_sum / total_time;
-    out_gz = g_z_sum / total_time;
-
-    std::cout << "\nAverage Gyro (rad/s): " << out_gx << ", " << out_gy << ", " << out_gz << std::endl;
-
+    
     return 0;
 }
